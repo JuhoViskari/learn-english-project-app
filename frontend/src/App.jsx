18cb93202f31edb3b195a4bf2c83860a6c943d9a
Juho Viskari <juho.viskari@tuni.fi>
2023-12-18 15:13:05 +0200

    frontend: fetch all. Root:  Dockerfile, .dockerignore, docker-compose

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  let [learning, setLearning] = useState([]);
+  const fetchIt = async () => {
+    let hr = await fetch("http://localhost:8080/api/learn");
+    let data = await hr.json();
+    setLearning(data);
+  };
+
+  let arr = learning.map((learning) => (
+    <li key={learning.id}>
+      Finnish: {learning.finnish}, english: {learning.english}
+    </li>
+  ));
+
+  return (
+    <>
+      <h1>Learnings</h1>
+      <div id="learning">{arr}</div>
+      <button onClick={fetchIt}>Fetch</button>
+    </>
+  );
+}
+
+export default App;
