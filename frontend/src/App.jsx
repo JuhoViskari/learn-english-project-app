884c7bfc6bfa62795b8d3bbf836cc844e7e339b9
Juho Viskari <juho.viskari@tuni.fi>
2023-12-19 12:08:24 +0200

    app.jsx modify to basic check will render deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import PropTypes from "prop-types";
 
-function LearnEnglish({ learning }) {
-  return (
-    <div id="learningenglish">
-      {/* Map through learning array and display each item */}
-      <h1>Suomesta englanniksi</h1>
-      {learning.map((item) => (
-        <div key={item.id}>
-          {item.finnish} = {""} {item.english}
-        </div>
-      ))}
-    </div>
-  );
-}
-LearnEnglish.propTypes = {
-  learning: PropTypes.array.isRequired,
-};
-
-function LearnFinnish({ learning }) {
-  return (
-    <div id="learningfinnish">
-      {/* Map through learning array and display each item */}
-      <h1>English to Finnish</h1>
-      {learning.map((item) => (
-        <div key={item.id}>
-          {item.english} = {""} {item.finnish}
-        </div>
-      ))}
-    </div>
-  );
-}
-
-LearnFinnish.propTypes = {
-  learning: PropTypes.array.isRequired,
-};
 function App() {
   let [learning, setLearning] = useState([]);
 
@@ -43,8 +7,8 @@ function App() {
     const fetchAll = async () => {
       try {
         //developing fetch
-        // const response = await fetch("http://localhost:8080/api/learn");
-        const response = await fetch("/api/learn");
+        const response = await fetch("http://localhost:8080/api/learn");
+        // const response = await fetch("/api/learn");
         const data = await response.json();
         setLearning(data);
       } catch (error) {
@@ -57,24 +21,28 @@ function App() {
   }, []);
 
   return (
-    <Router>
-      <div className="topnav">
-        <Link to="/" className="active">
-          Learn English
-        </Link>
-        <Link to="/learn-finnish">Learn Finnish</Link>
-        <Link to="/admin">Admin</Link>
-      </div>
+    <>
+      <div id="learningenglish">
+        {/* Map through learning array and display each item */}
+        <h1>Suomesta englanniksi</h1>
+        {learning.map((item) => (
+          <div key={item.id}>
+            {item.finnish} = {""} {item.english}
+          </div>
+        ))}
 
-      <Routes>
-        <Route path="/" element={<LearnEnglish learning={learning} />} />
-        <Route
-          path="/learn-finnish"
-          element={<LearnFinnish learning={learning} />}
-        />
-        {/* <Route path="/admin" element={<LearnEnglish learning={learning} />} /> */}
-      </Routes>
-    </Router>
+        <div id="learningfinnish">
+          {/* Map through learning array and display each item */}
+          <h1>English to finnish</h1>
+          {learning.map((item) => (
+            <div key={item.id}>
+              {item.english} = {""} {item.finnish}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
+
 export default App;
