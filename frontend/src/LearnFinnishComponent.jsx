3ae641cf66c1fa2928f1a261b47eacb7f362d37b
Juho Viskari <juho.viskari@tuni.fi>
2023-12-19 21:13:39 +0200

    modify:LearnFinnishComponent, add statevariable quessFinnish function handlequesses and inputfields

--- frontend/src/LearnFinnishComponent.jsx
+++ frontend/src/LearnFinnishComponent.jsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from "react";
 
 function LearnFinnish() {
-  let [learning, setLearning] = useState([]);
+  const [learning, setLearning] = useState([]);
+  const [quessFinnish, setQuessFinnish] = useState([]);
 
   useEffect(() => {
     const fetchAll = async () => {
       try {
         //developing fetch
-        // const response = await fetch("http://localhost:8080/api/learn");
-        const response = await fetch("/api/learn");
+        const response = await fetch("http://localhost:8080/api/learn");
+        // const response = await fetch("/api/learn");
         const data = await response.json();
         setLearning(data);
+        // Reset the current question when fetching new data
+        setQuessFinnish(Array(data.lenght).fill(""));
       } catch (error) {
         console.error("Error fetching tasks:", error);
       }
@@ -20,13 +23,24 @@ function LearnFinnish() {
     fetchAll();
   }, []);
 
+  const handlequesses = (index, value) => {
+    const newGuesses = [...quessFinnish];
+    newGuesses[index] = value;
+    setQuessFinnish(newGuesses);
+  };
   return (
     <div id="learninfinnish">
       {/* Map through learning array and display each item */}
       <h1>English to finnish</h1>
-      {learning.map((item) => (
+      {learning.map((item, index) => (
         <div key={item.id}>
-          {item.english} = {""} {item.finnish}
+          {item.english} = {""}
+          {/* {input to text quessing english word} */}
+          <input
+            type="text"
+            value={quessFinnish[index]}
+            onChange={(e) => handlequesses(index, e.target.value)}
+          ></input>
         </div>
       ))}
     </div>
