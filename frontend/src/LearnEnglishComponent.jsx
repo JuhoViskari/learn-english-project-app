43fc0378331b39d865d0835dc11894bb2971a06d
Juho Viskari <juho.viskari@tuni.fi>
2023-12-19 16:49:55 +0200

    bcrypt decpencie

--- frontend/src/LearnEnglishComponent.jsx
+++ frontend/src/LearnEnglishComponent.jsx
@@ -7,8 +7,8 @@ function LearnEnglish() {
     const fetchAll = async () => {
       try {
         //developing fetch
-        // const response = await fetch("http://localhost:8080/api/learn");
-        const response = await fetch("/api/learn");
+        const response = await fetch("http://localhost:8080/api/learn");
+        // const response = await fetch("/api/learn");
         const data = await response.json();
         setLearning(data);
       } catch (error) {
