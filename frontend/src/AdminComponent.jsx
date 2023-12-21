d5e77f434414fb9b3c245fd98b7ccfbd7b12a03f
Juho Viskari <juho.viskari@tuni.fi>
2023-12-21 11:12:29 +0200

    modify: AdminComponent: Add HTTP POST to add words with error state variables and english, finnish statevariables, inputs to page, handle inputChange function also

--- frontend/src/AdminComponent.jsx
+++ frontend/src/AdminComponent.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
+// const response = await fetch("http://localhost:8080/api/learn");
+// const response = await fetch("/api/learn");
 const AdminPage = () => {
+  const [english, setEnglish] = useState("");
+  const [finnish, setFinnish] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState(null);
+
+  // styling Admin page
   const AboutPageStyle = {
     textAlign: "center",
     padding: "20px",
@@ -8,11 +16,61 @@ const AdminPage = () => {
     backgroundColor: "lightgray",
   };
 
+  // styling Admin page
   const AboutPageParagStyle = {
     textAlign: "center",
     padding: "20px",
     border: "solid",
     backgroundColor: "lightblue",
+  };
+
+  // handle input change if word is same
+  // add value with state variable
+  const handleInputChange = (index) => {
+    const { word, value } = index.target;
+    if (word === "finnish") {
+      setFinnish(value);
+    } else if (word === "english") {
+      setEnglish(value);
+    }
+  };
+
+  // HTTP POST TO ADD WORDS Function
+  const PostWords = async () => {
+    try {
+      // if finnish and english have no text throw error
+      if (!finnish || !english) {
+        throw new Error("Finnish and English words are required");
+      }
+
+      // else show confirm alert to proceed
+      const result = window.confirm(
+        `Do you want to add Words with Finnish: ${finnish}, English: ${english}?`
+      );
+
+      // http fetch if result OK make http post
+      if (result) {
+        const response = await fetch(`/api/learn`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ finnish, english }),
+        });
+
+        // if hr not OK throw error
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        // else add word succesfully show to user
+        setMessage("Words added successfully");
+        setError(null);
+        console.log("Successful POST request");
+      }
+    } catch (error) {
+      setError(error.message);
+      console.error("Error during POST request", error);
+    }
   };
 
   return (
@@ -28,6 +86,28 @@ const AdminPage = () => {
         Tässä on minun ottama kuva Tammerkoskesta keväällä 2023 copyright: Juho
         Viskari
       </p>
+      <h1>ADD WORDS</h1>
+      <input
+        type="text"
+        id={`finnish`}
+        name={`finnish`}
+        placeholder="Enter Finnish word"
+        value={finnish}
+        onChange={handleInputChange}
+      />
+      <br />
+      <input
+        type="text"
+        id={`english`}
+        name={`english`}
+        placeholder="Enter English word"
+        value={english}
+        onChange={handleInputChange}
+      />
+      <br />
+      <button onClick={PostWords}>Add Words</button>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <p>{message}</p>
     </div>
   );
 };
