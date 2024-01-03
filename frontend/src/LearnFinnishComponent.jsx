1cd7db89b16f8ea5ccc7a2716af1dda25ebfc2d6
Juho Viskari <juho.viskari@tuni.fi>
2024-01-03 15:56:43 +0200

    modify LearnFinnishComponent to make same as learnenglishcomponent but different way

--- frontend/src/LearnFinnishComponent.jsx
+++ frontend/src/LearnFinnishComponent.jsx
@@ -3,7 +3,10 @@ import React, { useState, useEffect } from "react";
 function LearnFinnish() {
   const [learning, setLearning] = useState([]);
   const [quessFinnish, setQuessFinnish] = useState([]);
-
+  const [feedback, setFeedback] = useState([]);
+  const [countCorrectAnswers, setCountCorrectAnswers] = useState([]);
+  // total count of questions
+  const TotalQuestions = learning.length;
   useEffect(() => {
     const fetchAll = async () => {
       try {
@@ -11,9 +14,15 @@ function LearnFinnish() {
         // const response = await fetch("http://localhost:8080/api/learn");
         const response = await fetch("/api/learn");
         const data = await response.json();
-        setLearning(data);
-        // Reset the current question when fetching new data
-        setQuessFinnish(Array(data.lenght).fill(""));
+        // call randomized data
+        const shuffledLearning = shuffle(data);
+        // intialize 10 limit
+        const showTenItems = shuffledLearning.slice(0, 10);
+        setLearning(showTenItems);
+        // Reset the current question when fetching new random data
+        setQuessFinnish(Array(showTenItems.length).fill(""));
+        // reset correct answers when fetch new data
+        setCountCorrectAnswers(0);
       } catch (error) {
         console.error("Error fetching tasks:", error);
       }
@@ -23,26 +32,80 @@ function LearnFinnish() {
     fetchAll();
   }, []);
 
+  // handle quess field
   const handlequesses = (index, value) => {
     const newGuesses = [...quessFinnish];
     newGuesses[index] = value;
     setQuessFinnish(newGuesses);
   };
+  // check is value correct or incorrect map item and index
+  const handleCheckButton = () => {
+    const newFeedback = learning.map((item, index) => {
+      const value = quessFinnish[index].toLowerCase();
+      if (value === item.finnish.toLowerCase()) {
+        //increment correct answers
+        setCountCorrectAnswers(countCorrectAnswers + 1);
+        return "✅";
+      } else {
+        return `❌ ${item.finnish}`;
+      }
+    });
+
+    // state variable get parameter newFeedBack
+    setFeedback(newFeedback);
+    // if fb === with correct answers filter lenght
+    const newCountCorrectAnswers = newFeedback.filter(
+      (fb) => fb === "✅"
+    ).length;
+    setCountCorrectAnswers(newCountCorrectAnswers);
+  };
+
+  // randomize words to quess
+  const shuffle = (array) => {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
+  };
+
   return (
-    <div id="learninfinnish">
+    <div id="learningenglish">
+      <h1>English to Finnish</h1>
       {/* Map through learning array and display each item */}
-      <h1>English to finnish</h1>
       {learning.map((item, index) => (
-        <div key={item.id}>
-          {item.english} = {""}
-          {/* {input to text quessing english word} */}
+        <div key={item.id} style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ marginRight: "10px" }}>{item.english}</div>
+          {/* Input to guess */}
           <input
             type="text"
+            id={`questin-${index}`}
+            name={`question-${index}`}
             value={quessFinnish[index]}
-            onChange={(e) => handlequesses(index, e.target.value)}
-          ></input>
+            onChange={(e) => handlequesses(index, e.target.value, item)}
+          />
+          <span style={{ marginLeft: "10px" }}>{feedback[index]}</span>
+          {/* Display total correct answers */}
+          {/*show feedback correct wrong */}
         </div>
       ))}
+      <button onClick={handleCheckButton}>Tarkista</button>
+      <p>
+        Oikein vastatut: {countCorrectAnswers}/{TotalQuestions}
+      </p>
+      <p style={{ fontSize: "50px" }}>
+        {countCorrectAnswers === 0
+          ? " "
+          : countCorrectAnswers < 5
+          ? "😵‍💫"
+          : countCorrectAnswers < 10
+          ? "😎"
+          : "🎉🤩"}
+      </p>
     </div>
   );
 }
